62447fede0ca21e498aa0c5a56ae610cede50757
chrisn27 <chris_nguyen27@yahoo.com>
2017-06-17 15:56:11 -0700

    yes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,34 +4,30 @@ var express = require('express');
 var router = express.Router();
 
 
-router.post('/api/friends', function (req, res) {
-	var data = friends.get();
-	console.log(data);
-	return res.json(data);
-});
-
 router.get('/api/friends', function (req, res) {
 	var data = friends.get();
 	console.log(data);
 	return res.json(data);
 });
 
+// router.get('/api/friends', function (req, res) {
+// 	var data = friends.get();
+// 	console.log(data);
+// 	return res.json(data);
+// });
+
 router.post("/api/new/friends", function (req, res) {
     var newFriend = req.body;
     console.log(newFriend);
     friends.push(newFriend);
-    res.json(newFriend);
+    return res.json(newFriend);
 });
 
-router.get("/api/best/friends", function (req, res) {
+router.post("/api/friends", function (req, res) {
 
 var data = friends.get();
 
-var bestMatch = {
-			name: "",
-			photo: "",
-			friendDifference: 1000
-		};
+var bestMatch = data[0];
 
 		var userData 	= req.body;
 		var userName 	= userData.name;
@@ -41,20 +37,18 @@ var bestMatch = {
 		var totalDifference = 0;
 
 		for(var i = 0; i < friends.length-1; i++){
-			console.log(friends[i].name);
 			totalDifference = 0;
-
 			for(var j = 0; j < 10; j++){
-	
 				totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
 				if (totalDifference <= bestMatch.friendDifference){
-	 
+					console.log("non blank bff");
 					bestMatch.name = friends[i].name;
 					bestMatch.photo = friends[i].photo;
 					bestMatch.friendDifference = totalDifference;
 				}
 			}
 		} 
+		console.log(bestMatch)
 		return res.json(bestMatch);
 });
 
